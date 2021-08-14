6f1d5ee6f5072aacc8b70593a70ea97dfc1a28fa
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 06:24:32 +0900

    開始日から終了日まで回すループ

--- src/collect/modules/scraping.py
+++ src/collect/modules/scraping.py
@@ -1,15 +1,19 @@
 import datetime
 
 
-def exec(start_date_str, end_date_str):
-    start_date = parse_date(start_date_str)
-    end_date = parse_date(end_date_str)
-    return True
-
-
 # yyyy-MM-ddと言う形式の文字列をdatetime型に変換する
 def parse_date(date):
     year = int(date[:4])
     month = int(date[5:7])
     day = int(date[8:10])
     return datetime.date(year, month, day)
+
+
+# 実行
+def exec(start_date_str, end_date_str):
+    current_date = parse_date(start_date_str)
+    end_date = parse_date(end_date_str)
+    while current_date <= end_date:
+        current_date += datetime.timedelta(days=1)
+
+    return True
