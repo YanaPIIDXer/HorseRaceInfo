560c5baabeb5247020facf1f8c082eafd9420932
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 04:55:47 +0900

    開催場所を列挙してみるテスト

--- src/app/mainview.py
+++ src/app/mainview.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from collect.models import CourseInfo
+
 
 def index(request):
-    return render(request, "main/index.html")
+    coursers = CourseInfo.objects.all().order_by("id")
+    params = {
+        'courses': coursers
+    }
+    return render(request, "main/index.html", params)
