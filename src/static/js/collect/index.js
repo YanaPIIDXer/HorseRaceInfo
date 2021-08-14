46c87fbddad59503c93b113e8d6b51de68cb004b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 06:05:08 +0900

    一週間前から当日までを初期値とするようにした

--- src/static/js/collect/index.js
+++ src/static/js/collect/index.js
@@ -1,3 +1,18 @@
+window.onload = () => {
+    const start = new Date();
+    const end = new Date();
+    start.setDate(end.getDate() - 7);
+    $("#start_date").val(dateToString(start));
+    $("#end_date").val(dateToString(end));
+}
+
+const dateToString = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return year + "-" + ("00" + month).slice(-2) + "-" + ("00" + day).slice(-2);
+}
+
 const onSubmit = () => {
     const result = window.confirm("情報収集を開始しますか？");
     if (!result) { return; }
