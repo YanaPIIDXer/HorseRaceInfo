bd86ff7a07a75282a9896cdcbd84bd6b837f031a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 04:57:35 +0900

    不要なimportも撤去

--- src/app/mainview.py
+++ src/app/mainview.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from collect.models import CourseInfo
-
 
 def index(request):
     return render(request, "main/index.html")
