2ae8adc3a20d716b55e2a6ae0d868bc3127cb9ad
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 05:03:58 +0900

    とりあえずスクレイピングを行う期間を投げてトップページにリダイレクトするだけのものを実装

--- src/collect/views.py
+++ src/collect/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, "collect/index.html")
+
+
+def collect(request):
+    return redirect("/")
