216b9fe7eaa99cd793408384395d39236135b48a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 07:16:14 +0900

    リファクタリング

--- src/collect/modules/scraping.py
+++ src/collect/modules/scraping.py
@@ -10,13 +10,15 @@ def configure_race_id_list():
 
     # 過去５年間分に遡って取得する
     # 参考：https://qiita.com/dijzpeb/items/434b259e473cc8646e91
-    year = datetime.date.today().year - 5
-    for current in range(year, year + 6):
+    year_range = 1  # 時間がかかりすぎるので一時的に１年分としている
+    start_year = datetime.date.today().year - year_range
+    end_year = start_year + year_range
+    for year in range(start_year, end_year + 1):
         for course_id in range(1, 11):
             for kai in range(1, 6):
                 for day in range(1, 9):
                     for round in range(1, 13):
-                        id = str(current) + str(course_id).zfill(2) + \
+                        id = str(year) + str(course_id).zfill(2) + \
                             str(kai).zfill(2) + str(day).zfill(2) + \
                             str(round).zfill(2)
                         id_list.append(int(id))
