d68a2fd6720bcf74a0f5996292fe07131b636595
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-15 04:20:02 +0900

    競走馬情報追加

--- src/collect/models.py
+++ src/collect/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+# 競争馬情報
+class HorseInfo(models.Model):
+    name = models.CharField(max_length=64, blank=False,
+                            null=False, unique=True)
 
 
 # レース情報
@@ -6,3 +13,4 @@ class RaceInfo(models.Model):
     date = models.DateField()
     place = models.CharField(max_length=16, blank=False, null=False)
     round = models.IntegerField(blank=False, null=False)
+    horces = models.ManyToManyField(HorseInfo, related_name="horces")
